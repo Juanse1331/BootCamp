8d1afbf750e095be958b7754202b614d3d735edf
Juan Suarez <juan2201993@correo.uis.edu.co>
2024-09-10 22:03:20 -0500

    Feat:final de la clase 1

---
+++ src/components/botones-calc.jsx
@@ -0,0 +1,9 @@
+
+function DigitButton ({onClick, digit}) {
+    return (
+        <button onClick={(() => onClick(digit))}>
+            {digit}
+        </button>
+    )
+}
+export default DigitButton;
